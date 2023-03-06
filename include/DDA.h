c2a9751b52c2654594e8a3ef3b6eb64350ee9989
Pepe Ojeda <ojedamorala@hotmail.com>
2023-03-06 12:20:36 +0100

    added a templated definition for the functions
    You can now pass a 2D vector of arbitrary type + a predicate, rather than a vector of bools

--- include/DDA.h
+++ include/DDA.h
@@ -7,6 +7,22 @@
 
 namespace DDA
 {
+    static void resetColor () {
+        printf("\033[0m");
+    }
+
+    static void Error () {
+        printf("\033[1;31m");
+        printf("[ERROR] ");
+        resetColor();
+    }
+
+    static void Warn() {
+        printf("\033[1;33m");
+        printf("[WARN] ");
+        resetColor();
+    }
+    
     struct RayCastInfo
     {
         bool hitSomething;
@@ -20,9 +36,118 @@ namespace DDA
         RayMarchInfo(std::unordered_map<glm::ivec2, float> inputMap): lengthInCell(std::move(inputMap)){}
     };
 
-    //returns true if a blocked cell was hit. The outline of the map is considered blocked.
-    RayCastInfo castRay(const glm::vec2& start, glm::vec2 direction, const float maxDistance, const std::vector<std::vector<bool>>& map, const glm::vec2& mapOrigin, const float mapResolution);
-    //returns how far through each cell the ray has traveled. Useful for volumetric calculations
-    RayMarchInfo marchRay(const glm::vec2& start, glm::vec2 direction, const float maxDistance, const std::vector<std::vector<bool>>& map, const glm::vec2& mapOrigin, const float mapResolution);
+    //returns true if a blocked cell was hit. The outline of the map is considered blocked.    
+    template<typename T>
+    RayCastInfo castRay(const glm::vec2& start, glm::vec2 direction, const float maxDistance, const std::vector<std::vector<T>>& map, std::function<bool(T)> predicate, const glm::vec2& mapOrigin, const float mapResolution)
+    {
+        if(glm::length(direction) == 0)
+        {
+            Warn();
+            printf("Ray of length 0\n");
+            return {false,0};
+        }
+        
+        glm::ivec2 currentCell = (start-mapOrigin)/mapResolution;
+        if( !predicate(map[currentCell.x][currentCell.y]) )
+        {
+            Error();
+            printf("Ray outside the environment!\n"); 
+            return {false,0};
+        }
+        glm::vec2 currentPosition = start;
 
+        direction = direction/glm::length(direction);
+        int stepX = glm::sign(direction.x);
+        int stepY = glm::sign(direction.y);
+        
+        float currentDistance = 0;
+        while(true)
+        {
+            float xCoordNext = (stepX>0? currentCell.x+1 : currentCell.x) * mapResolution + mapOrigin.x;
+            float yCoordNext = (stepY>0? currentCell.y+1 : currentCell.y) * mapResolution + mapOrigin.y;
+            float tMaxX = (xCoordNext-currentPosition.x)/direction.x;
+            float tMaxY = (yCoordNext-currentPosition.y)/direction.y;
+            if((stepX!=0 && tMaxX<tMaxY)|| stepY==0)
+            {
+                currentPosition += direction * tMaxX;
+                currentCell += glm::ivec2{stepX, 0};
+                currentDistance += tMaxX;
+            }
+            else
+            {
+                currentPosition += direction * tMaxY;
+                currentCell += glm::ivec2{0, stepY};
+                currentDistance += tMaxY;
+            }
+
+            if(glm::length(currentPosition-start)>maxDistance)
+                return {false, maxDistance};
+            else if (currentCell.x<0 || currentCell.x>=map.size()
+                    || currentCell.y<0 || currentCell.y>=map[0].size()
+                    || !predicate( map[currentCell.x][currentCell.y] ) )
+                return {true, currentDistance};
+        }
+    }
+    
+    //returns how far through each cell the ray has traveled. Useful for volumetric calculations
+    template <typename T>
+    RayMarchInfo marchRay(const glm::vec2& start, glm::vec2 direction, const float maxDistance, const std::vector<std::vector<T>>& map,std::function<bool(T)> predicate, const glm::vec2& mapOrigin, const float mapResolution)
+    {
+        if(glm::length(direction) == 0)
+        {
+            Warn();
+            printf("Ray of length 0\n");
+
+            return RayMarchInfo();
+        }
+        
+        glm::ivec2 currentCell = (start-mapOrigin)/mapResolution;
+        
+        if(!predicate( map[currentCell.x][currentCell.y] ) )
+        {
+            Error();
+            printf("Ray outside the environment!\n");   
+
+            return RayMarchInfo();
+        }
+
+        glm::vec2 currentPosition = start;
+
+        direction = direction/glm::length(direction);
+        int stepX = glm::sign(direction.x);
+        int stepY = glm::sign(direction.y);
+        
+        float currentDistance = 0;
+        std::unordered_map<glm::ivec2, float> lengthsMap; 
+        while(true)
+        {
+            float xCoordNext = (stepX>0? currentCell.x+1 : currentCell.x) * mapResolution + mapOrigin.x;
+            float yCoordNext = (stepY>0? currentCell.y+1 : currentCell.y) * mapResolution + mapOrigin.y;
+            float tMaxX = (xCoordNext-currentPosition.x)/direction.x;
+            float tMaxY = (yCoordNext-currentPosition.y)/direction.y;
+            if((stepX!=0 && tMaxX<tMaxY)|| stepY==0)
+            {
+                if(tMaxX>0)
+                    lengthsMap.insert({currentCell, tMaxX});
+                currentPosition += direction * tMaxX;
+                currentCell += glm::ivec2{stepX, 0};
+                currentDistance += tMaxX;
+            }
+            else
+            {
+                if(tMaxY>0)
+                    lengthsMap.insert({currentCell, tMaxY});
+                currentPosition += direction * tMaxY;
+                currentCell += glm::ivec2{0, stepY};
+                currentDistance += tMaxY;
+            }
+
+            if(glm::length(currentPosition-start)>maxDistance)
+                return RayMarchInfo();
+            else if (currentCell.x<0 || currentCell.x>=map.size()
+                    || currentCell.y<0 || currentCell.y>=map[0].size()
+                    || !predicate( map[currentCell.x][currentCell.y] ) )
+                return {lengthsMap};
+        }
+    }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -16,13 +16,13 @@ void test()
 
     std::cout<<"Raycast:\n";
     {
-        auto result = DDA::castRay({0.5, 0.5}, {0,1}, 10, map, {0,0}, 1);
+        auto result = DDA::castRay<bool>({0.5, 0.5}, {0,1}, 10, map, [](bool b){return b;}, {0,0}, 1);
         std::cout<<result.hitSomething<<" "<<result.distance<<"m\n";
     }
 
     std::cout<<"Raymarch:\n";
     {
-        auto result = DDA::marchRay({2.7, 0.5}, {1,1}, 10, map, {0,0}, 1);
+        auto result = DDA::marchRay<bool>({2.7, 0.5}, {1,1}, 10, map,[](bool b){return b;}, {0,0}, 1);
         for(const auto& p : result.lengthInCell)
         {
             glm::ivec2 cell =p.first;
